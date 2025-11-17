d5d8aaa27dae8870c55ca9a990108ecf9e2299e2
pratik2061 <pratiksharma2061@gmail.com>
2025-11-17 16:27:39 +0545

    :endpoints

--- apiEndpoint.ts
+++ apiEndpoint.ts
@@ -1,14 +1,14 @@
 export const apiEndpoint = {
-  addGallery: "http://api.sebs.edu.np/api/gallery/add",
-  fetchGallery: "http://api.sebs.edu.np/api/gallery",
+  addGallery: "https://api.sebs.edu.np/api/gallery/add",
+  fetchGallery: "https://api.sebs.edu.np/api/gallery",
   deleteGallery: (id: number) =>
-    `http://api.sebs.edu.np/api/gallery/delete/${id}`,
-  addNews: "http://api.sebs.edu.np/api/news/add",
-  fetchNews: "http://api.sebs.edu.np/api/news",
+    `https://api.sebs.edu.np/api/gallery/delete/${id}`,
+  addNews: "https://api.sebs.edu.np/api/news/add",
+  fetchNews: "https://api.sebs.edu.np/api/news",
   deleteNews: (id: number) =>
-    `http://api.sebs.edu.np/api/news/delete/${id}`,
-  addfaculty: "http://api.sebs.edu.np/api/faculty/add",
-  fetchFaculty: "http://api.sebs.edu.np/api/faculty",
+    `https://api.sebs.edu.np/api/news/delete/${id}`,
+  addfaculty: "https://api.sebs.edu.np/api/faculty/add",
+  fetchFaculty: "https://api.sebs.edu.np/api/faculty",
   deleteFaculty: (id: number) =>
-    `http://api.sebs.edu.np/api/faculty/delete/${id}`,
+    `https://api.sebs.edu.np/api/faculty/delete/${id}`,
 };
