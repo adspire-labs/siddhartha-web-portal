9350378cc3c9f4f7ef63443e89975038f94b36d7
pratik sharma <123287083+pratik2061@users.noreply.github.com>
2025-12-19 16:08:47 +0545

    Update API endpoint URLs to include trailing slash

--- apiEndpoint.ts
+++ apiEndpoint.ts
@@ -1,13 +1,13 @@
 export const apiEndpoint = {
-  addGallery: "https://api.sebs.edu.np/api/gallery/add",
-  fetchGallery: "https://api.sebs.edu.np/api/gallery",
-  deleteGallery: (id: number) =>`https://api.sebs.edu.np/api/gallery/delete/${id}`,
-  addNews: "https://api.sebs.edu.np/api/news/add",
-  fetchNews: "https://api.sebs.edu.np/api/news",
+  addGallery: "https://api.sebs.edu.np/api/gallery/add/",
+  fetchGallery: "https://api.sebs.edu.np/api/gallery/",
+  deleteGallery: (id: number) =>`https://api.sebs.edu.np/api/gallery/delete/${id}/`,
+  addNews: "https://api.sebs.edu.np/api/news/add/",
+  fetchNews: "https://api.sebs.edu.np/api/news/",
   deleteNews: (id: number) =>
-    `https://api.sebs.edu.np/api/news/delete/${id}`,
-  addfaculty: "https://api.sebs.edu.np/api/faculty/add",
-  fetchFaculty: "https://api.sebs.edu.np/api/faculty",
+    `https://api.sebs.edu.np/api/news/delete/${id}/`,
+  addfaculty: "https://api.sebs.edu.np/api/faculty/add/",
+  fetchFaculty: "https://api.sebs.edu.np/api/faculty/",
   deleteFaculty: (id: number) =>
-    `https://api.sebs.edu.np/api/faculty/delete/${id}`,
+    `https://api.sebs.edu.np/api/faculty/delete/${id}/`,
 };
